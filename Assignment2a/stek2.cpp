8d73e6620c8259924cb4629f72a0d18840ff360d
Konstantinst128027 <kostaberezkin403@gmail.com>
2024-10-09 00:26:42 +0300

    I added function that overloads the array

---
+++ Assignment2a/stek2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+void reverseArray(char arr[], int  size) 
+    {    
+         char c;
+         for (int i = 0; i < ((size-2)/2); i++)
+             {
+                 c =  arr[i];
+                 arr[i] = arr[size - 2 - i];
+                 arr[size - 2 - i] = c;
+             }
+    }
