9c99afbddb53b26a4e3a75ba55705f6797d1e486
Konstantinst128027 <kostaberezkin403@gmail.com>
2024-10-24 23:16:14 +0300

    I added class Transformer

---
+++ Assignment3/main.cpp
@@ -0,0 +1,8 @@
+/* Berezkin Konstantin Evgenievich
+./assignment3 st128027@student.spbu.ru*/
+#include "transformer.h"
+int main() 
+    {
+        std::cout<<"its worked"<<std::endl;
+        return 0;
+    }
