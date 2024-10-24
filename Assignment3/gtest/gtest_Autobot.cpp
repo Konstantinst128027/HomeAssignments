34b8c79ecc3edf9a10e0501bbc37643c48c50495
Konstantinst128027 <kostaberezkin403@gmail.com>
2024-10-24 23:14:24 +0300

    I added gtests

---
+++ Assignment3/gtest/gtest_Autobot.cpp
@@ -0,0 +1,34 @@
+/* Berezkin Konstantin Evgenievich
+./assignment3 st128027@student.spbu.ru*/
+#include "gtest/gtest.h"
+#include "Autobot.h"
+
+TEST(Autobot, get_motto) 
+{
+    Autobot autobot("Optimus prime",100,"Goida");
+    EXPECT_EQ(autobot.get_motto(),"Goida");
+}
+TEST(Autobot, get_have_a_matrix_of_leadership) 
+{
+    Autobot autobot("Optimus prime",100,"Goida");
+    EXPECT_EQ(autobot.get_have_a_matrix_of_leadership(),true);
+}
+TEST(Autobot, set_motto) 
+{
+    Autobot autobot("Optimus prime",100,"Goida");
+    EXPECT_EQ(autobot.get_motto(),"Goida");
+    autobot.set_motto("We are autobots!");
+    EXPECT_EQ(autobot.get_motto(),"We are autobots!");
+}
+TEST(Autobot, set_have_a_matrix_of_leadership) 
+{
+    Autobot autobot("Optimus prime",100,"Goida");
+    EXPECT_EQ(autobot.get_have_a_matrix_of_leadership(),true);
+    autobot.set_have_a_matrix_of_leadership(false);
+    EXPECT_EQ(autobot.get_have_a_matrix_of_leadership(),false);
+}
+TEST(Autobot, Driving) 
+{
+    Autobot autobot("Optimus prime",100,"Goida");
+    EXPECT_EQ(autobot.Driving(), true);
+}
---
+++ Assignment3/gtest/gtest_armour.cpp
@@ -0,0 +1,29 @@
+/* Berezkin Konstantin Evgenievich
+./assignment3 st128027@student.spbu.ru*/
+#include "gtest/gtest.h"
+#include "armour.h"
+
+TEST(Armour, get_armour_name) 
+{
+    Armour armour("Shield");
+    EXPECT_EQ(armour.get_armour_name(),"Shield");
+}
+TEST(Armour, get_strenght) 
+{
+    Armour armour("Shield");
+    EXPECT_EQ(armour.get_strenght(),100);
+}
+TEST(Armour, set_armour_name) 
+{
+    Armour armour("Shield");
+    EXPECT_EQ(armour.get_armour_name(),"Shield");
+    armour.set_armour_name("blademail");
+    EXPECT_EQ(armour.get_armour_name(),"blademail");
+}
+TEST(Armour, set_strenght) 
+{
+    Armour armour("Shield");
+    EXPECT_EQ(armour.get_strenght(),100);
+    armour.set_strenght(30);
+    EXPECT_EQ(armour.get_strenght(),30);
+}
