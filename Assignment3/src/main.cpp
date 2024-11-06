73d5012d295259c8a78b8c21c0c066e1bff33f25
Konstantinst128027 <kostaberezkin403@gmail.com>
2024-11-06 19:04:07 +0300

    i crrected all mistakes

---
+++ Assignment3/src/main.cpp
@@ -0,0 +1,8 @@
+/* Berezkin Konstantin Evgenievich
+./assignment3 st128027@student.spbu.ru*/
+#include "transformer.h"
+#include "gtest/gtest.h"
+int main() {
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
